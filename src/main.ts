955a676ceb07656ac8ce1cc1f46fc9c212651f8d
JinSeongLee <84770467+ljin1599@users.noreply.github.com>
2022-11-03 11:09:41 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,21 +11,18 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import router  from './router'
 
-
-
-
 const app = createApp(App)
 
-axios.defaults.baseURL = `http://localhost:3001`    // axios 기본 주소 지정
+axios.defaults.baseURL = `http://localhost:3001` // axios 기본 주소 지정
 
-app.use(createPinia( ))   // pinia
+app.use(createPinia()) // pinia
 
-app.use(ElementPlus)    // elementplus
+app.use(ElementPlus) // elementplus
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)   // elementplus 아이콘 사용
+    app.component(key, component) // elementplus 아이콘 사용
 }
 
-app.use(router)             // router
+app.use(router) // router
 
 app.mount('#app')

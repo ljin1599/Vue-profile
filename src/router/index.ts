fdc85ae528c246749b93fad561f2d350a3e1b878
JinSeongLee <84770467+ljin1599@users.noreply.github.com>
2022-11-03 11:08:43 +0900

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,45 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Main from '@/views/Main.vue'
-import addAbout from '@/views/addAbout.vue'
 import About from '@/views/About.vue'
-import readAbout from '@/views/readAbout.vue'
 import Skill from '@/views/Skill.vue'
-import addSkill from '@/views/addSkill.vue'
-import readSkill from '@/views/readSkill.vue'
 import Frontend from '@/views/Frontend.vue'
 import Backend from '@/views/Backend.vue'
 import Database from '@/views/Database.vue'
 import Project from '@/views/Project.vue'
-import addProject from '@/views/addProject.vue'
-import readProject from '@/views/readProject.vue'
 import Career from '@/views/Career.vue'
-import addCareer from '@/views/addCareer.vue'
+import readAbout from '@/views/readAbout.vue'
+import readSkill from '@/views/readSkill.vue'
+import readProject from '@/views/readProject.vue'
 import readCareer from '@/views/readCareer.vue'
 
 export const routes = [
     {path: '/',  components: {
-        a: Main,
-        b: addAbout,
+        a: Main
         },
         props: true
     },
-    {path: '/addAbout', name: 'addabout', component: addAbout, props: true},
-    {path: '/about', name: 'about', component:  About, props: true},
-    {path: '/readabout', name:'readabout', component: readAbout, props: true},
+    {path:'/about', name: 'about', component:  About, props: true},
     {path:'/skill', name: 'skill', component: Skill, props: true},
-    {path:'/addskill', name: 'addskill', component: addSkill, props: true},
-    {path:'/readskill', name: 'readskill', component: readSkill, props: true},
     {path:'/frontend', name: 'frontend', component: Frontend, props: true},
     {path:'/backend', name: 'backend', component: Backend, props: true},
     {path:'/database', name: 'database', component: Database, props: true},
     {path:'/project', name: 'project', component: Project, props: true},
-    {path:'/addproject', name: 'addproject', component: addProject, props: true},
-    {path:'/readproject', name: 'readproject', component: readProject, props: true},
     {path:'/career', name: 'career', component: Career, props: true},
-    {path:'/addcareer', name: 'addcareer', component: addCareer, props: true},
+    {path:'/readabout', name:'readabout', component: readAbout, props: true},
+    {path:'/readskill', name: 'readskill', component: readSkill, props: true},
+    {path:'/readproject', name: 'readproject', component: readProject, props: true},
     {path:'/readcareer', name: 'readcareer', component: readCareer, props: true},
-    ]
+]
 
 const router = createRouter({  // 기록모드 HTML5
     history: createWebHistory(), // 페이지를 다시 로드하지 않고 URL을 탐색할 수 있습니다.
